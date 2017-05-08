15bb7e61c5d7c43c7b5902835ed2181d52d3bc59
jonathanc56 <jonathanc@orelcorp.com>
2017-05-08 12:06:53 +0530

    Initial

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
  * Provides access to basic micro:bit functionality.
  */
 
-//% color=#000000 weight=100 icon="\uf1ec" block="Jonathan"
+//% color=#000000 weight=100 icon="<i class="lab icon"></i>" block="Jonathan"
 namespace jonathan 
 {
 

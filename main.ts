7c78131b01d1381ec68f1705d256d42cc4691a1b
jonathanc56 <jonathanc@orelcorp.com>
2017-05-08 11:36:19 +0530

    Initial

--- main.ts
+++ main.ts
@@ -2,12 +2,12 @@
  * Provides access to basic micro:bit functionality.
  */
 
-//% color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
+//% color=190 weight=100 icon="\uf1ec" block="Jonathan"
 namespace jonathan 
 {
 
 //% block
-export function allLED(v: number, interval: number = 150): void
+export function allLED(): void
 {
     basic.showLeds
     (`
--- projects/Untitled/main.ts
+++ projects/Untitled/main.ts
@@ -1,7 +1,10 @@
+jonathan.allLED(0, 0)
+basic.pause(1000)
 basic.showLeds(`
-    # # # # #
-    # # # # #
-    # # # # #
-    # # # # #
-    # # # # #
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
     `)
+basic.pause(1000)

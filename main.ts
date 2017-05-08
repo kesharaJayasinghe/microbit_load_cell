faa2cceba8bada280b8650ec43b98052a5489148
jonathanc56 <jonathanc@orelcorp.com>
2017-05-08 11:31:58 +0530

    Initial

--- main.ts
+++ main.ts
@@ -3,5 +3,21 @@
  */
 
 //% color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
-namespace jonathan {
+namespace jonathan 
+{
+
+//% block
+export function allLED(v: number, interval: number = 150): void
+{
+    basic.showLeds
+    (`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+
+ }
+
 }

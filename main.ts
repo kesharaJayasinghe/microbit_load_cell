809f3bada39a94f4c02890c994851d4c8bea86e9
jonathanc56 <jonathanc@orelcorp.com>
2017-09-23 19:22:30 +0530

    yay

--- main.ts
+++ main.ts
@@ -20,7 +20,7 @@ export function readTemp(): void
 }
 
 //% block    
- export function setTempThreshold(duration: number)): void
+ export function setTempThreshold(duration: number): void
 {
     basic.showLeds
     (`

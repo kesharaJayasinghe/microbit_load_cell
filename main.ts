0485f636bf5e7ab81df11c540f350c2cf7593379
jonathanc56 <jonathanc@orelcorp.com>
2017-12-02 18:14:13 +0530

    Done

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 /**
- * Provides access to basic micro:bit functionality.
+ * Allows interface with the HX711 Load Cell Board
  */
 
-//% color=#000000 weight=100 icon="\uf013" block="Orel - Temp Sensor"
-namespace jonathan {
+//% color=#000000 weight=100 icon="\uf013" block="Orel - Load Cell"
+namespace Load_Cell {
 
     //% block
     export function readTemp(): void {
@@ -17,14 +17,41 @@ namespace jonathan {
     `)
     }
 
+
+
     //% block    
-    export function setTempThreshold(value: number): void {
-        basic.showLeds
-            (`
-    # # # # #
-    # # # # #
-    # # # # #
-    # # # # #
-    # # # # #
-    `)
+    export function readValue(value: number): void {
+
+        let j = 0
+        let count = 0
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        count = 0
+
+
+        while (pins.digitalReadPin(DigitalPin.P1) == 1) {
+
+        }
+
+        count = 0
+        while (j < 24) {
+            pins.digitalWritePin(DigitalPin.P0, 1)
+            control.waitMicros(5)
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            control.waitMicros(5)
+            basic.pause(1)
+            count = count << 1;
+            if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+                count = count + 1
+            }
+        }
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        control.waitMicros(5)
+        count = count ^ 0x800000
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        serial.writeString("Weight Value:")
+        serial.writeNumber(count)
+        serial.writeLine("")
+        serial.writeLine("")
+
     }
+}
